83b75b5ec0657648f5298793fe7501f9038b9792
chewyt <yeetian.chew@gmail.com>
2022-01-03 00:51:03 +0800

    first commit

---
+++ demo/src/main/java/chewyt/demo/SsfDay12Application.java
@@ -0,0 +1,13 @@
+package chewyt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfDay12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfDay12Application.class, args);
+	}
+
+}
---
+++ demo/src/test/java/chewyt/demo/SsfDay12ApplicationTests.java
@@ -0,0 +1,13 @@
+package chewyt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfDay12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
